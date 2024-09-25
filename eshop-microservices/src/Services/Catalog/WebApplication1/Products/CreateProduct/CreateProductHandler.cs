68f7286fd8e6ab944b6d87e4e4737f573dc26759
Poolfx1872 <ptorres@metropolitan-touring.com>
2024-09-24 22:57:20 -0500

    create buildingblock

--- eshop-microservices/src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,36 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+using System.Xml.Linq;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // create Product entity from command object
+            //save to database
+            //return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //todo
+            //Save Options to data base
+
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
