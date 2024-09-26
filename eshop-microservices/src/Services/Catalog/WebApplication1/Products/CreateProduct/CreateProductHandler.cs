ff3a9893deb05fcc72b88a4214da51870eebf73f
Poolfx1872 <ptorres@metropolitan-touring.com>
2024-09-25 22:36:21 -0500

    configure dokcer compose

--- eshop-microservices/src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
-using System.Xml.Linq;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -29,7 +24,10 @@ namespace Catalog.API.Products.CreateProduct
 
             //todo
             //Save Options to data base
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
+            //return result
             return new CreateProductResult(Guid.NewGuid());
         }
     }
