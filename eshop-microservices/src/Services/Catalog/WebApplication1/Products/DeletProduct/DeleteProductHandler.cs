fd4dd662e50038abf0e8f1ab29480ccf7fa8275b
Poolfx1872 <ptorres@metropolitan-touring.com>
2024-10-01 20:46:17 -0500

    create update an delet product

---
+++ eshop-microservices/src/Services/Catalog/WebApplication1/Products/DeletProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Catalog.API.Products.GetProductByCategory;
+
+namespace Catalog.API.Products.DeletProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccedd);
+
+public class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
